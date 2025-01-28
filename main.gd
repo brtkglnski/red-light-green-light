87a1c0ab3827e6507c7d602ecacae7bd8bfca62e
brtkglnski <glinskib2008@gmail.com>
2025-01-28 12:13:00 +0100

    Full functionality

--- main.gd
+++ main.gd
@@ -5,43 +5,66 @@ extends Node2D
 @onready var watcher = $Watcher
 @onready var timer = $Timer
 @onready var goal = $Goal
-#@onready var ui_label = $UI/Label
+@onready var successScreen = $SuccessScreen
+@onready var eliminatedScreen = $eliminatedScreen
+@onready var SpawnLocation = $SpawnLocation
 
 # Game state variables
 var game_running = true
-var player_is_moving = false
 func _ready():
 	# Start the timer for green/red light
 	timer.start()
 	print("ready function activated")
-	print("Player Collision Layer: ", player.collision_layer)
-	print("Player Collision Mask: ", player.collision_mask)
-	print("Goal Collision Layer: ", goal.collision_layer)
-	print("Goal Collision Mask: ", goal.collision_mask)
-	print("Player position: ", player.global_position)
-	print("Goal position: ", goal.global_position)
 
 func _on_timer_timeout():
 	print("timer timeout function activated")
 	if game_running:
 		watcher.change_state()
 
-func _on_watcher_state_changed(is_green_light):
-	print("on watcher state changed function activated")
-	if not is_green_light and player_is_moving:
-		game_over()
 
+func _physics_process(delta):
+	if game_running:
+		while watcher.is_light_green():
+			if Input.is_action_pressed("up"):
+				game_over()
+				break
+			else:
+				break
+			
 func game_over():
 	print("game over function activated")
 	game_running = false
-	#ui_label.text = "Game Over! You moved during red light!"
+	player.set_physics_process(false)
+	player.stop_animation()
+	player.play_animation("death") 
+	eliminatedScreen.visible = true
 	timer.stop()
 
 func _on_goal_area_entered(p):
-	# Check if the player reaches the goal
 	print("on goal area entered")
 	if game_running:
 		print("you won")
+		watcher.countdown.stop()
 		game_running = false
-		#ui_label.text = "You Win!"
+		player.set_physics_process(false)
+		player.stop_animation()
+		successScreen.visible = true
 		timer.stop()
+
+
+func _on_button_pressed() -> void:
+	player.global_position = SpawnLocation.global_position
+	player.set_physics_process(true)
+	game_running = true
+	eliminatedScreen.visible = false
+	successScreen.visible = false
+	player.play_animation("walk")
+	if watcher.is_light_green():
+		watcher.change_state()
+	if not watcher.is_light_green():
+		watcher.countdown.play()
+	timer.start()
+
+
+func _on_goal_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
--- movement.gd
+++ movement.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-@export var speed = 100 
+@export var speed = 30
 @onready var animated_sprite = $CollisionShape2D/AnimatedSprite2D 
+var player_is_moving = false
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
@@ -11,10 +12,15 @@ func _physics_process(delta):
 	move_and_slide()
 
 	if velocity.y != 0:  
+		player_is_moving = true
 		play_animation("walk") 
 	else:
+		player_is_moving = false
 		stop_animation()
 
+func is_player_moving() -> bool:
+	return player_is_moving
+	
 func play_animation(animation_name: String):
 	if not animated_sprite.is_playing():
 		animated_sprite.play(animation_name)
